4ea5b0291bbd42635b8fc789423f3a6264b392d6
Nicholas L <NLoo1@outlook.com>
2024-04-10 14:52:44 -0400

    add invoices and companies

--- db.js
+++ db.js
@@ -1,19 +1,24 @@
-/** Database setup for BizTime. */
-
-const { Client } = require("pg");
+const { Client } = require('pg');
 
 let DB_URI;
-
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///biztime_test";
+  DB_URI = "biztime_test"
 } else {
-  DB_URI = "postgresql:///biztime";
+  DB_URI = "biztime"
 }
 
-let db = new Client({
-  connectionString: DB_URI
+// These are default credentials that should be replaced 
+const db = new Client({
+  user: 'N',
+  host: 'localhost',
+  password: 'abc123',
+  database: DB_URI,
+  port: 5432, 
 });
 
-db.connect();
+// Connect to the database
+db.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Error connecting to PostgreSQL database', err));
 
-module.exports = db;
+module.exports = db
